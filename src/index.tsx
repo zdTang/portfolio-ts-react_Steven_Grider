06da846f470cdeadcae4d3b2fa065264b471fccd
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-11 17:31:37 -0500

    006 Changes with TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import Parent from "./props/Parent";
 
 export const App = () => {
   return (
     <div>
       <h1>Hi there!</h1>
+      <Parent />
     </div>
   );
 };
---
+++ src/props/Parent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Child from "./Child";
+
+const Parent = () => {
+  return <Child />;
+};
+
+export default Parent;
