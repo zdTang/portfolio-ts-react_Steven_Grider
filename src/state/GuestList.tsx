7c20da4fc4096033324e680a58541062d95a475b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-12 08:10:55 -0500

    011 Type Inference with State
    Typescript cannot inference what type you are using, so we need to give him a hint
    
    useState<string[]>([]);

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 
 const GuestList: React.FC = () => {
   const [guest, setGuest] = useState("");
+  const [guestList, setGuestList] = useState<string[]>([]);
 
   const addGuestHandler = () => {
     console.log(guest);
+    setGuestList((prev) => [...prev, guest]);
+    setGuest("");
   };
 
   const inputHandler = (e: any) => {
     setGuest(e.target.value);
   };
+
   return (
     <div>
       <h3>Guest List</h3>
