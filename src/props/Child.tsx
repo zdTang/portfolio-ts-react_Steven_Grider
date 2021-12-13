f53f499330bd16067c2653996589e880529866db
Zhendong Tang <zhendongtt@gmail.com>
2021-12-13 09:48:03 -0500

    add comment

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 // in the child component, define a interface for props
+// this interface need a object and have a key called COLOR
 interface ChildProps {
   color: string;
 }
 
+//const Child = (color: ChildProps) => {// wrong
+// here , the props we need is an object
+// here, by using destruct, we can use color directly
 const Child = ({ color }: ChildProps) => {
   return (
     <div>
